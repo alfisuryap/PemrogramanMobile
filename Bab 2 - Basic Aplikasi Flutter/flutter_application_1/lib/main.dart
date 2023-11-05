946e3e19a389c6d259f98169c00730e75628e00a
alfisuryap <alfisuryap7@gmail.com>
2023-11-05 08:40:28 +0700

    commit tugas

--- Bab 2 - Basic Aplikasi Flutter/flutter_application_1/lib/main.dart
+++ Bab 2 - Basic Aplikasi Flutter/flutter_application_1/lib/main.dart
@@ -5,153 +5,103 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class TabBarDemo extends StatelessWidget {
-  const TabBarDemo({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 3,
+        length: 2,
         child: Scaffold(
           appBar: AppBar(
-            bottom: const TabBar(
+            backgroundColor: Color.fromARGB(255, 244, 0, 0),
+            title: Text("MyApp"),
+            bottom: TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(
+                  text: 'Berita',
+                ),
+                Tab(
+                  text: 'Pertandingan Hari ini',
+                ),
               ],
             ),
-            title: const Text('Tabs Demo'),
           ),
-          body: const TabBarView(
+          body: TabBarView(
             children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
+              Container(
+                width: 200.0,
+                height: 300.0,
+                child: Column(
+                  children: [
+                    Image.network(
+                      'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt8f381d19d9c8d246/60dc02d820a5380ed1a4489e/b21af15a62b1f217b7e68c5b183e50f8facb7109.jpg?auto=webp&format=pjpg&width=3840&quality=60',
+                      fit: BoxFit.cover, // Mengatur tampilan gambar
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 5),
+                      child: Text(
+                        'Costa Mendekat Ke Palmaeiras', // Teks di bawah gambar
+                        style: TextStyle(
+                          fontSize: 16.0, // Ukuran teks
+                          fontWeight: FontWeight.bold, // Ketebalan teks
+                        ),
+                      ),
+                    ),
+                    Container(
+                      color: Color.fromARGB(255, 179, 0, 202),
+                      width: double.infinity,
+                      padding: EdgeInsets.only(top: 10, bottom: 10),
+                      margin: EdgeInsets.only(top: 5, bottom: 20),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: 5, // Ganti dengan jumlah item yang sesuai
+                        itemBuilder: (context, index) {
+                          final itemImage =
+                              'https://ichef.bbci.co.uk/news/640/cpsprodpb/356F/production/_128397631_gettyimages-1286178456.jpg';
+                          final itemTitle =
+                              'Pique Bilang Wasit Untungkan Madrid, Koeman Tepok Jidat';
+                          final itemDate = '20-08-2023';
+                          return ListTile(
+                            leading: Image.network(
+                              itemImage,
+                              // Atur lebar gambar sesuai keinginan
+                              width: 150,
+                              fit: BoxFit.cover, // Mengatur tampilan gambar
+                            ),
+                            title: Text(
+                              itemTitle,
+                              style: TextStyle(
+                                fontSize:
+                                    12.0, // Atur ukuran font sesuai keinginan
+                              ),
+                            ),
+                            subtitle: Text(
+                              itemDate,
+                              style: TextStyle(
+                                fontSize:
+                                    12.0, // Atur ukuran font sesuai keinginan
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                child: Text('Konten Pertandingan Hari ini'),
+              ),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
